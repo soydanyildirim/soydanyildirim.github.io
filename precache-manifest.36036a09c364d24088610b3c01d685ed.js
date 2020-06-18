deaeb22291f68edf9f219928cb5c832ac274a92c
Soydan YILDIRIM <soydann.yildirim@gmail.com>
2020-06-18 23:36:59 +0300

    Create

---
+++ precache-manifest.36036a09c364d24088610b3c01d685ed.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e8b7275cd063232b970c13ab2a281b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72f4bfa7e5804d0536ec",
+    "url": "/static/css/2.b211811c.chunk.css"
+  },
+  {
+    "revision": "79bd6f2950feab45909c",
+    "url": "/static/css/main.fce3790b.chunk.css"
+  },
+  {
+    "revision": "72f4bfa7e5804d0536ec",
+    "url": "/static/js/2.71bb287a.chunk.js"
+  },
+  {
+    "revision": "8bfe196eb7a5edd3b2b3b5b6a33efb39",
+    "url": "/static/js/2.71bb287a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79bd6f2950feab45909c",
+    "url": "/static/js/main.47cb10b9.chunk.js"
+  },
+  {
+    "revision": "1aa9aa2e31a68ae1e121",
+    "url": "/static/js/runtime-main.79bf7f08.js"
+  },
+  {
+    "revision": "bc29c5fa035b64d7e8813a04e08a4a8c",
+    "url": "/static/media/Soydan_YILDIRIM.bc29c5fa.zip"
+  },
+  {
+    "revision": "0883bacf8c9a06fcac836afd8340bf57",
+    "url": "/static/media/home-background.0883bacf.jpg"
+  }
+]);
